efd7ca37a9cef25942a65430275d7fec307dc825
kjkandrea <kjkandrea@gmail.com>
2024-07-01 13:24:13 +0900

    feat. loading component

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
+
+async function getMovies() {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+
+export default async function HomePage() {
+    const movies = await getMovies()
+    return <div>{JSON.stringify(movies)}</div>
+}
